daa1f1dd86a3de53aacd7ba921ce9404360f7861
Pree <siktheday@gmail.com>
2023-12-15 21:39:39 -0500

    fixed cat easter egg

--- commands/nap.js
+++ commands/nap.js
@@ -1,22 +1,20 @@
 //connect to Cats
 function getCat() {
-    fetch(`https://cataas.com/cat`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(`error ${err}`);
-      });
-  }
-  
-  //Bot instructions
-  module.exports = {
-      name: 'nap',
-      description: 'sends cat photo and encourages nap',
-      execute(message, args) {
-        message.channel.send(CAT IMG)
-      }
-  
-  }
-  
+  fetch(`https://cataas.com/cat`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+//Bot instructions
+module.exports = {
+  name: "nap",
+  description: "sends cat photo and encourages nap",
+  execute(message, args) {
+    message.channel.send(`https://cataas.com/cat`);
+  },
+};
